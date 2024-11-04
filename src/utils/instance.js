e401bfee0643d975d0676d02fa70f4ec4a56c2f3
Ing. Armando Campos <58012193+armando19993@users.noreply.github.com>
2024-11-04 13:14:55 -0400

    Topdos los ajustres

--- src/utils/instance.js
+++ src/utils/instance.js
@@ -3,26 +3,28 @@ import Cookies from 'js-cookie';
 
 // Crear instancia básica sin token
 const instance = axios.create({
-    baseURL: 'https://api.jansprogramming.com.co/api/',
+  baseURL: 'https://api.jansprogramming.com.co/api/',
+  baseURL: 'http://localhost:4000/api/',
 });
 
 // Crear instancia con token
 const instanceWithToken = axios.create({
-    baseURL: 'https://api.jansprogramming.com.co/api/',
+  baseURL: 'https://api.jansprogramming.com.co/api/',
+  baseURL: 'http://localhost:4000/api/',
 });
 
 // Añadir el token a los headers de cada solicitud
 instanceWithToken.interceptors.request.use(
-    (config) => {
-        const token = Cookies.get('token');
-        if (token) {
-            config.headers['Authorization'] = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = Cookies.get('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export { instance, instanceWithToken };
