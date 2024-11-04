a6f767b802f5c2289c4ac19685d7c1590f45fa5f
Ing. Armando Campos <58012193+armando19993@users.noreply.github.com>
2024-11-04 17:36:42 -0400

    fix: upload

--- src/sections/lotes/lote-table-row.jsx
+++ src/sections/lotes/lote-table-row.jsx
@@ -14,6 +14,7 @@ import { instanceWithToken } from 'src/utils/instance';
 import { toast } from 'react-toastify';
 import { format } from 'date-fns';
 import { useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 // ----------------------------------------------------------------------
 
